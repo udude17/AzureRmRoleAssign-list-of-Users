17705e98f0f0d220cf9129825102d30b40493c1e
udude17 <udude17@users.noreply.github.com>
2016-11-21 14:27:38 +0100

    Add files via upload

---
+++ AzureRm_AddUserRoleToSubAndToResGroup-SIMPLE.ps1
@@ -0,0 +1,42 @@
+﻿#
+Login-AzureRMAccount 
+$SubID  = "abcdef1234" #  
+Set-AzureRmContext -SubscriptionId $SubID 
+
+$RoleName1 = "Contributor"
+$RoleName2 = "Reader"
+
+# USER LIST (ARRAY) 1
+$UserArray1 = `
+    "user1_outlook.com#EXT#@azureadtenant.onmicrosoft.com",`
+    "user2_hotmail.com#EXT#@azureadtenant.onmicrosoft.com", `
+    "user3@azureadtenant1.onmicrosoft.com"
+
+# USER LIST (ARRAY) 2  
+$UserArray2 = `
+    "user4_outlook.com#EXT#@azureadtenant1.onmicrosoft.com",`
+    "user5@azureadtenant1.onmicrosoft.com"
+
+
+# ASSIGN ARRAY OF USERS to SPECIFIC RESOURCE GROUPS -----------------------------------
+$ResGroup1 = "RG-1"
+$ResGroup2 = "RG-2"
+$ResGroup3 = "RG-3"
+foreach ($user in $UserArray1)
+          { 
+          New-AzureRmRoleAssignment -SignInName $user -RoleDefinitionName $RoleName -ResourceGroupName $ResGroup1
+          New-AzureRmRoleAssignment -SignInName $user -RoleDefinitionName $RoleName -ResourceGroupName $ResGroup2
+          New-AzureRmRoleAssignment -SignInName $user -RoleDefinitionName $RoleName -ResourceGroupName $ResGroup3
+          }
+
+# ASSIGN USER to SUBSCRIPTION WITH BUILT-IN ROLE  -------------------------------------
+$subscope = "/subscriptions/"+$SubID
+foreach ($user in $UserArray2)
+        { 
+        New-AzureRmRoleAssignment -SignInName $user -Scope $subscope -RoleDefinitionName $RoleName1
+
+        }
+
+# DISPLAY ROLES FROM A RESOURCE GROUP --------------------------------------------------
+Get-AzureRmRoleAssignment -ResourceGroupName $ResGroup1 | FL DisplayName, SigninName, RoleDefinitionName, Scope
+
